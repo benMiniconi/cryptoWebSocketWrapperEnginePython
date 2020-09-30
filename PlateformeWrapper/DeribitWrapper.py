1dbfa48875a4390c1ecfe69c4fbd6bd0df9e598e
miniconi <benjamin.deplus@miniconicapital.ciom>
2020-09-30 18:56:39 +0200

    Deribit is now able to reconnect after lost connection Has to be extend to other plateforme

--- PlateformeWrapper/DeribitWrapper.py
+++ PlateformeWrapper/DeribitWrapper.py
@@ -11,29 +11,27 @@ msg = \
      "id": 8106,
      "params": {
          "instrument_name": "BTC-PERPETUAL"}
-}
-
-msgforEth =  {"jsonrpc": "2.0",
-     "method": "public/ticker",
-     "id": 8106,
-     "params": {
-         "instrument_name": "ETH-PERPETUAL"}
-}
-msgSuscribeToBTCUSD = {"jsonrpc": "2.0",
-     "method": "public/subscribe",
-     "id": 42,
-     "params": {
-         "channels": ["ticker.BTC-PERPETUAL.raw"]}
      }
 
+msgforEth = {"jsonrpc": "2.0",
+             "method": "public/ticker",
+             "id": 8106,
+             "params": {
+                 "instrument_name": "ETH-PERPETUAL"}
+             }
+msgSuscribeToBTCUSD = {"jsonrpc": "2.0",
+                       "method": "public/subscribe",
+                       "id": 42,
+                       "params": {
+                           "channels": ["ticker.BTC-PERPETUAL.raw"]}
+                       }
 
 msgSuscribeToETHUSD = {"jsonrpc": "2.0",
-     "method": "public/subscribe",
-     "id": 42,
-     "params": {
-         "channels": ["ticker.ETH-PERPETUAL.raw"]}
-     }
-
+                       "method": "public/subscribe",
+                       "id": 42,
+                       "params": {
+                           "channels": ["ticker.ETH-PERPETUAL.raw"]}
+                       }
 
 hearbeat = \
     {
@@ -46,16 +44,17 @@ hearbeat = \
     }
 
 getInstruments = {
-  "jsonrpc" : "2.0",
-  "id" : 7617,
-  "method" : "public/get_instruments",
-  "params" : {
-    "currency" : "BTC",
-    "kind" : "index",
-    "expired" : False
-  }}
+    "jsonrpc": "2.0",
+    "id": 7617,
+    "method": "public/get_instruments",
+    "params": {
+        "currency": "BTC",
+        "kind": "index",
+        "expired": False
+    }}
 
 websocket = ""
+websocket2 = ""
 buffer = []
 
 
@@ -64,11 +63,15 @@ def cleanAssetName(rawAssetName):
     rawAssetNameClean = rawAssetNameClean.replace("PERPETUAL", "USD")
     return rawAssetNameClean.lower()
 
+
 def prepareJson(rawDeribitJson):
     if "params" in rawDeribitJson.keys():
         data = rawDeribitJson["params"]["data"]
         cleanName = cleanAssetName(data["instrument_name"])
-        quote = {"Plateforme": "Deribit", "Asset": cleanName, "Quote": data["index_price"], "Datetime": int(data["timestamp"])/1000, "Bid": data["best_bid_price"], "BidAmount": data["best_bid_amount"], "Ask":  data["best_ask_price"], "AskAmount": data["best_ask_amount"], "OpenInterest": data["open_interest"]}
+        quote = {"Plateforme": "Deribit", "Asset": cleanName, "Quote": data["index_price"],
+                 "Datetime": int(data["timestamp"]) / 1000, "Bid": data["best_bid_price"],
+                 "BidAmount": data["best_bid_amount"], "Ask": data["best_ask_price"],
+                 "AskAmount": data["best_ask_amount"], "OpenInterest": data["open_interest"]}
         return quote
 
 
@@ -88,22 +91,73 @@ def emptyBuffer():
     buffer = []
 
 
+async def reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD):
+    print("in reconnect")
+    global websocket
+    websocket = ""
+    async with websockets.connect('wss://www.deribit.com/ws/api/v2', ping_interval=None) as websocket2:
+        print("after re-connection", websocket2)
+        await websocket2.send(msgSuscribeToBTCUSD)
+        await websocket2.send(msgSuscribeToETHUSD)
+        while websocket2.open:
+            # await websocket.send(suscribeToBTCUSD)
+            try:
+                response = await websocket2.recv()
+                response_json = json.loads(response)
+                #print("reconnect message.recv")
+                # print(response_json)
+                csv_file = "crypto" + "Deribit" + moment.now().format("DDMMYYYY") + ".csv"
+                quote = prepareJson(response_json)
+
+                if quote: csvWriter.writeQuote(csv_file, [quote])
+                manageBuffer(quote)
+            except websockets.exceptions.ConnectionClosedOK:
+                print("ConnectionClosedOK")
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
+            except websockets.exceptions.ConnectionClosedError:
+                print("ConnectionClosedError")
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
+            except websockets.exceptions.ConnectionClosed:
+                print("ConnectionClosedOK")
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
+
+
 async def call_api(msgSuscribeToBTCUSD, msgSuscribeToETHUSD):
+    global websocket2
+    websocket2 = ""
     async with websockets.connect('wss://www.deribit.com/ws/api/v2', ping_interval=None) as websocket:
+        print("after connection", websocket2)
         await websocket.send(msgSuscribeToBTCUSD)
         await websocket.send(msgSuscribeToETHUSD)
         while websocket.open:
-            #await websocket.send(suscribeToBTCUSD)
-            response = await websocket.recv()
-            response_json = json.loads(response)
-            #print(response_json)
-            csv_file = "crypto"+"Deribit"+moment.now().format("DDMMYYYY")+".csv"
-            quote = prepareJson(response_json)
-            if quote: csvWriter.writeQuote(csv_file, [quote])
-            manageBuffer(quote)
-
+            # await websocket.send(suscribeToBTCUSD)
+            try:
+                response = await websocket.recv()
+                response_json = json.loads(response)
+                # print(response_json)
+                csv_file = "crypto" + "Deribit" + moment.now().format("DDMMYYYY") + ".csv"
+                quote = prepareJson(response_json)
+                if quote: csvWriter.writeQuote(csv_file, [quote])
+                manageBuffer(quote)
+            except websockets.exceptions.ConnectionClosedOK:
+                print("ConnectionClosedOK")
+                await websocket.close_connection()
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
+            except websockets.exceptions.ConnectionClosedError:
+                print("ConnectionClosedError")
+                await websocket.close_connection()
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
+            except websockets.exceptions.ConnectionClosed:
+                await websocket.close_connection()
+                print("ConnectionClosedOK")
+                await asyncio.sleep(3)
+                await reconnect(msgSuscribeToBTCUSD, msgSuscribeToETHUSD)
 
 
 asyncio.get_event_loop().run_until_complete(call_api(json.dumps(msgSuscribeToBTCUSD), json.dumps(msgSuscribeToETHUSD)))
-
-
+# asyncio.run(call_api(json.dumps(msgSuscribeToBTCUSD), json.dumps(msgSuscribeToETHUSD)))
